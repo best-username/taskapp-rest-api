6a728662d632d150b186343bc55f52ed8eda9e61
Andrii Yarosh <yarosh.andrey2001@gmail.com>
2020-07-09 13:48:00 +0300

    Refactor EventServiceProvider

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -5,7 +5,8 @@ namespace App\Providers;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
-use Illuminate\Support\Facades\Event;
+use App\Events\TaskUpdated;
+use App\Listeners\WriteToTaskLog;
 
 class EventServiceProvider extends ServiceProvider
 {
@@ -18,8 +19,8 @@ class EventServiceProvider extends ServiceProvider
         Registered::class => [
             SendEmailVerificationNotification::class,
         ],
-        'App\Events\TaskUpdated' => [
-            'App\Listeners\WriteToTaskLog',
+        TaskUpdated::class => [
+            WriteToTaskLog::class,
         ],
     ];
 
